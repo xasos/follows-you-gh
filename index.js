35f67295e8a4d1d78e98b1fb79143e370fdd4203
Niraj Pant <nirajpant7@gmail.com>
2015-12-01 22:09:40 -0600

    Fix follow logic

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   var TARGET_URL = "https://api.github.com/";
 
   if ( $('.vcard-fullname').length > 0 ) { // Page is profile page
-    var targetUser = $(this).attr('title').match(/(.*?) (.*?)/)[1];
-    var username = $(".css-truncate-target")[0].innerHTML;
+    var username = $(this).attr('title').match(/(.*?) (.*?)/)[1];
+    var targetUser = $(".css-truncate-target")[0].innerHTML;
     var requestURL = TARGET_URL + "users/" + username + "/following/" + targetUser; 
     $.get(requestURL, function(data, status) {
         $("<span class=\"repo-private-label\">FOLLOWS YOU</span>").insertAfter(".vcard-username");
